3ffe64640e8c0b03f7763869f30a6979e46128e4
joseEstudos <jose.games15@outlook.com>
2022-08-09 14:41:15 -0300

    vuejs

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
+
 new Vue({
     el: '#app',
+    data:{
+        playerLife:100,
+        monsterLife:100
+    },
+    computed:{
+
+    },
+    methods: {
+        
+    },
+    watch:{
+
+    }
    
 })
